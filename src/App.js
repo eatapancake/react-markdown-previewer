f609bbae9ad42d4bfc349fa4b880b64d286acad3
eatapancake <eat.a.pancake27@gmail.com>
2022-06-19 00:07:51 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { marked } from "marked";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import "./App.css";
+import placeholder from "./placeholder";
 
 function App() {
+  const renderer = new marked.Renderer();
+  renderer.link = function (href, title, text) {
+    return `<a target"_blank href="${href}">${text}</a>`;
+  };
+
+  const [message, setMessage] = useState(placeholder);
+
+  const textInputOnChange = (event) => {
+    setMessage(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="editor-container">
+          <h1>React Markdown</h1>
+          <textarea
+            id="editor"
+            value={message}
+            type="text"
+            onChange={textInputOnChange}
+          ></textarea>
+        </div>
+        <div id="preview-container">
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{
+              __html: marked(message, { renderer: renderer }),
+            }}
+          ></div>
+        </div>
       </header>
     </div>
   );
